435f4433f1c38a0ce4b61d029caf491677a72b7d
leo0263 <leo.0263@gmail.com>
2018-03-26 13:38:30 +0700

    Hooking the Retrofit call to MainActivity * Call the XkcdApiService from MainActivity * Bind the Retrofit response to xkcdData * Update the ui to see if xkcdData is successfully retrieved * Adding INTERNET permission

--- app/src/main/java/com/example/q1/belajarkotlinkedua/MainActivity.kt
+++ app/src/main/java/com/example/q1/belajarkotlinkedua/MainActivity.kt
@@ -2,11 +2,47 @@ package com.example.q1.belajarkotlinkedua
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.example.q1.belajarkotlinkedua.data.XkcdApiService
+import com.example.q1.belajarkotlinkedua.data.XkcdData
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var xkcdApiService : XkcdApiService
+    private lateinit var xkcdData : XkcdData
+
+    private lateinit var testText : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        testText = findViewById(R.id.text_test)
+
+        initRetrofit()
+        getLatestComic()
     }
+
+    private fun initRetrofit() {
+        xkcdApiService = XkcdApiService.create()
+    }
+
+
+    private fun getLatestComic() {
+        xkcdApiService.getLatestComic()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(
+                        {  xkcdData = it; updateUI() },
+                        { Log.e("MainActivity", it.toString()) }
+                )
+    }
+
+    private fun updateUI() {
+        testText.text = xkcdData.safe_title
+    }
+
 }
